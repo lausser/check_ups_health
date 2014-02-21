219da3da6f7a546291c45401a5ab40633c315c01
Gerhard Lausser <gerhard.lausser@consol.de>
2014-02-21 11:19:55 +0100

    more cleanup, bugfix in apc modes

--- plugins-scripts/Classes/APC/Powermib.pm
+++ plugins-scripts/Classes/APC/Powermib.pm
@@ -5,9 +5,9 @@ use strict;
 sub init {
   my $self = shift;
   if ($self->mode =~ /device::battery/) {
-    $self->analyze_and_check_battery_subsystem('Classes::APC::Powermib::Components::EnvironmentalSubsystem');
+    $self->analyze_and_check_battery_subsystem('Classes::APC::Powermib::Components::BatterySubsystem');
   } elsif ($self->mode =~ /device::hardware/) {
-    $self->analyze_and_check_environmental_subsystem('Classes::APC::Powermib::Components::BatterySubsystem');
+    $self->analyze_and_check_environmental_subsystem('Classes::APC::Powermib::Components::EnvironmentalSubsystem');
   } else {
     $self->no_such_mode();
   }
