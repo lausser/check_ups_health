21a0cac61bbffac0d692bb315fbb03405ad9a7ee
Gerhard Lausser <gerhard.lausser@consol.de>
2016-02-01 00:21:57 +0100

    Monitoring::GLPlugin raus

--- lib/Monitoring/GLPlugin/DB/Item.pm
+++
@@ -1,7 +0,0 @@
-package Monitoring::GLPlugin::DB::Item;
-our @ISA = qw(Monitoring::GLPlugin::DB::CSF Monitoring::GLPlugin::Item Monitoring::GLPlugin::DB);
-use strict;
-
-1;
-
-__END__
--- lib/Monitoring/GLPlugin/SNMP/Item.pm
+++
@@ -1,7 +0,0 @@
-package Monitoring::GLPlugin::SNMP::Item;
-our @ISA = qw(Monitoring::GLPlugin::SNMP::CSF Monitoring::GLPlugin::Item Monitoring::GLPlugin::SNMP);
-use strict;
-
-1;
-
-__END__
